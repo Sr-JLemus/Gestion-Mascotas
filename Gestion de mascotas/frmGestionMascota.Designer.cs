ea9deb9107c288f1638ef226d00ddffd3867e51e
Sr-JLemus <joselemus2006@outlook.com>
2025-09-29 10:06:49 -0600

    Form principal hecho

--- Gestion de mascotas/frmGestionMascota.Designer.cs
+++ Gestion de mascotas/frmGestionMascota.Designer.cs
@@ -29,49 +29,71 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGestionMascota));
-            pictureBox2 = new PictureBox();
-            pictureBox1 = new PictureBox();
+            BottomToolStripPanel = new ToolStripPanel();
+            TopToolStripPanel = new ToolStripPanel();
+            RightToolStripPanel = new ToolStripPanel();
+            LeftToolStripPanel = new ToolStripPanel();
+            ContentPanel = new ToolStripContentPanel();
             menuStrip1 = new MenuStrip();
             aNIMALESToolStripMenuItem = new ToolStripMenuItem();
             aDOPCIÓNToolStripMenuItem = new ToolStripMenuItem();
             dONATIVOSToolStripMenuItem = new ToolStripMenuItem();
             eVENTOSToolStripMenuItem = new ToolStripMenuItem();
             sALIRToolStripMenuItem = new ToolStripMenuItem();
-            toolStripContainer1 = new ToolStripContainer();
-            pictureBox4 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            label4 = new Label();
-            label3 = new Label();
-            label2 = new Label();
             label1 = new Label();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            toolStripContainer1 = new ToolStripContainer();
             menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             toolStripContainer1.ContentPanel.SuspendLayout();
             toolStripContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
-            // pictureBox2
+            // BottomToolStripPanel
             // 
-            pictureBox2.Image = Properties.Resources.cuidado_de_mascotas;
-            pictureBox2.Location = new Point(650, 73);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(411, 270);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 1;
-            pictureBox2.TabStop = false;
+            BottomToolStripPanel.Location = new Point(0, 0);
+            BottomToolStripPanel.Name = "BottomToolStripPanel";
+            BottomToolStripPanel.Orientation = Orientation.Horizontal;
+            BottomToolStripPanel.RowMargin = new Padding(4, 0, 0, 0);
+            BottomToolStripPanel.Size = new Size(0, 0);
             // 
-            // pictureBox1
+            // TopToolStripPanel
             // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(233, 73);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(411, 270);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            TopToolStripPanel.Location = new Point(0, 0);
+            TopToolStripPanel.Name = "TopToolStripPanel";
+            TopToolStripPanel.Orientation = Orientation.Horizontal;
+            TopToolStripPanel.RowMargin = new Padding(4, 0, 0, 0);
+            TopToolStripPanel.Size = new Size(0, 0);
+            // 
+            // RightToolStripPanel
+            // 
+            RightToolStripPanel.Location = new Point(0, 0);
+            RightToolStripPanel.Name = "RightToolStripPanel";
+            RightToolStripPanel.Orientation = Orientation.Horizontal;
+            RightToolStripPanel.RowMargin = new Padding(4, 0, 0, 0);
+            RightToolStripPanel.Size = new Size(0, 0);
+            // 
+            // LeftToolStripPanel
+            // 
+            LeftToolStripPanel.Location = new Point(0, 0);
+            LeftToolStripPanel.Name = "LeftToolStripPanel";
+            LeftToolStripPanel.Orientation = Orientation.Horizontal;
+            LeftToolStripPanel.RowMargin = new Padding(4, 0, 0, 0);
+            LeftToolStripPanel.Size = new Size(0, 0);
+            // 
+            // ContentPanel
+            // 
+            ContentPanel.AutoScroll = true;
+            ContentPanel.Size = new Size(1920, 1080);
             // 
             // menuStrip1
             // 
@@ -134,6 +156,90 @@
             sALIRToolStripMenuItem.Text = "SALIR";
             sALIRToolStripMenuItem.Click += sALIRToolStripMenuItem_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(237, 237, 237);
+            label1.Location = new Point(588, 483);
+            label1.Name = "label1";
+            label1.Size = new Size(156, 34);
+            label1.TabIndex = 3;
+            label1.Text = "ANIMALES";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold);
+            label2.ForeColor = Color.FromArgb(237, 237, 237);
+            label2.Location = new Point(1386, 483);
+            label2.Name = "label2";
+            label2.Size = new Size(184, 34);
+            label2.TabIndex = 4;
+            label2.Text = "ADOPCIONES";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold);
+            label3.ForeColor = Color.FromArgb(237, 237, 237);
+            label3.Location = new Point(588, 990);
+            label3.Name = "label3";
+            label3.Size = new Size(169, 34);
+            label3.TabIndex = 5;
+            label3.Text = "DONATIVOS";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold);
+            label4.ForeColor = Color.FromArgb(237, 237, 237);
+            label4.Location = new Point(1386, 990);
+            label4.Name = "label4";
+            label4.Size = new Size(136, 34);
+            label4.TabIndex = 6;
+            label4.Text = "EVENTOS";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.cuidado_de_mascotas;
+            pictureBox2.Location = new Point(1197, 35);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(647, 445);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(372, 542);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(647, 445);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(372, 35);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(647, 445);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(1197, 542);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(647, 445);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 1;
+            pictureBox4.TabStop = false;
+            // 
             // toolStripContainer1
             // 
             toolStripContainer1.BottomToolStripPanelVisible = false;
@@ -161,70 +267,6 @@
             toolStripContainer1.Text = "toolStripContainer1";
             toolStripContainer1.TopToolStripPanelVisible = false;
             // 
-            // pictureBox4
-            // 
-            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
-            pictureBox4.Location = new Point(650, 415);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(413, 270);
-            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox4.TabIndex = 1;
-            pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(232, 415);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(412, 270);
-            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox3.TabIndex = 0;
-            pictureBox3.TabStop = false;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold);
-            label4.ForeColor = Color.FromArgb(237, 237, 237);
-            label4.Location = new Point(747, 719);
-            label4.Name = "label4";
-            label4.Size = new Size(136, 34);
-            label4.TabIndex = 6;
-            label4.Text = "EVENTOS";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold);
-            label3.ForeColor = Color.FromArgb(237, 237, 237);
-            label3.Location = new Point(372, 719);
-            label3.Name = "label3";
-            label3.Size = new Size(169, 34);
-            label3.TabIndex = 5;
-            label3.Text = "DONATIVOS";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold);
-            label2.ForeColor = Color.FromArgb(237, 237, 237);
-            label2.Location = new Point(747, 346);
-            label2.Name = "label2";
-            label2.Size = new Size(184, 34);
-            label2.TabIndex = 4;
-            label2.Text = "ADOPCIONES";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.FromArgb(237, 237, 237);
-            label1.Location = new Point(372, 346);
-            label1.Name = "label1";
-            label1.Size = new Size(156, 34);
-            label1.TabIndex = 3;
-            label1.Text = "ANIMALES";
-            // 
             // frmGestionMascota
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -241,34 +283,40 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "frmGestionMascota";
             WindowState = FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             toolStripContainer1.ContentPanel.ResumeLayout(false);
             toolStripContainer1.ContentPanel.PerformLayout();
             toolStripContainer1.ResumeLayout(false);
             toolStripContainer1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ToolStripPanel BottomToolStripPanel;
+        private ToolStripPanel TopToolStripPanel;
+        private ToolStripPanel RightToolStripPanel;
+        private ToolStripPanel LeftToolStripPanel;
+        private ToolStripContentPanel ContentPanel;
         private MenuStrip menuStrip1;
         private ToolStripMenuItem aNIMALESToolStripMenuItem;
         private ToolStripMenuItem aDOPCIÓNToolStripMenuItem;
         private ToolStripMenuItem dONATIVOSToolStripMenuItem;
-        private ToolStripContainer toolStripContainer1;
         private ToolStripMenuItem eVENTOSToolStripMenuItem;
         private ToolStripMenuItem sALIRToolStripMenuItem;
-        private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
-        private Label label4;
-        private Label label3;
-        private Label label2;
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox2;
         private PictureBox pictureBox3;
+        private PictureBox pictureBox1;
         private PictureBox pictureBox4;
+        private ToolStripContainer toolStripContainer1;
     }
 }
